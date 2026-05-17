c9f7c55d376bee164f596e225ad1dd0b5530caa1
FelixSansores <josa9g9@hotmail.com>
2026-05-16 20:18:55 -0600

    se agregó AdminController. UserManagementController. Crear usuarios con rol desde el sistema. Lista de usuarios con sus roles.

---
+++ SGS.Web/Controllers/AdminController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace SGS.Web.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SGS.Web/Controllers/UserManagementController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SGS.Web.Models;
+
+namespace SGS.Web.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class UserManagementController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public UserManagementController(
+            UserManager<IdentityUser> userManager,
+            RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var users = _userManager.Users.ToList();
+
+            var userList = new List<(IdentityUser User, IList<string> Roles)>();
+
+            foreach (var user in users)
+            {
+                var roles = await _userManager.GetRolesAsync(user);
+                userList.Add((user, roles));
+            }
+
+            return View(userList);
+        }
+        public IActionResult CreateUser()
+        {
+            ViewBag.Roles = _roleManager.Roles.ToList();
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateUser(CreateUserViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewBag.Roles = _roleManager.Roles.ToList();
+                return View(model);
+            }
+
+            var user = new IdentityUser
+            {
+                UserName = model.Email,
+                Email = model.Email
+            };
+
+            var result = await _userManager.CreateAsync(user, model.Password);
+
+            if (result.Succeeded)
+            {
+                await _userManager.AddToRoleAsync(user, model.Role);
+                return RedirectToAction(nameof(Index));
+            }
+
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+            }
+
+            ViewBag.Roles = _roleManager.Roles.ToList();
+            return View(model);
+        }
+    }
+}
